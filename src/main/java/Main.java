ab01d74d31992296b85c049b4bccd3ada4e64971
DeathAwaits664 <ilai.rav664@gmail.com>
2020-03-26 17:31:44 +0300

    add JavaDoc3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-
         TaskHandler tl = new TaskHandler("src/main/resources/todo-list.json");
         ConsoleReader cr = new ConsoleReader(tl);
         cr.start();
